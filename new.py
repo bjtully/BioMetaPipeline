2d186a44bf1907a626b89cdef394e7005d23e1dc
Chris Neely <Christopher.neely1200@gmail.com>
2019-03-28 12:35:34 -0700

    Remove example config

--- new.py
+++
@@ -1,6 +0,0 @@
-from BioMetaPipeline.Accessories.DataStructures.linked_list import LinkedList
-
-ll = LinkedList()
-for i in range(10000):
-	ll.append(0)
-print(len(ll))
--- old.py
+++
@@ -1,4 +0,0 @@
-to_print = []
-for i in range(10000):
-	to_print.append(0)
-print(len(to_print))
