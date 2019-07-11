7e0c8d7130a80be9f9dc1ebecaa02bef81b0a963
Chris Neely <Christopher.neely1200@gmail.com>
2019-07-11 12:41:48 -0700

    Add mapping classes and output suffixes for available pipes in pipeline

--- BioMetaPipeline/Config/task_mapper.pyx
+++ BioMetaPipeline/Config/task_mapper.pyx
@@ -1,11 +1,18 @@
 # Contains mappings between name of pipeline and the task classes it contains
+from BioMetaPipeline.Annotation.biodata import BioDataConstants
 from BioMetaPipeline.Annotation.interproscan import InterproscanConstants
+from BioMetaPipeline.Annotation.prokka import PROKKAConstants
 from BioMetaPipeline.Annotation.virsorter import VirSorterConstants
 
 # dict will contain:
 #       "pipe_name": ("suffix_of_pipe_output"),
+from BioMetaPipeline.Peptidase.cazy import CAZYConstants
+from BioMetaPipeline.Peptidase.peptidase import PeptidaseConstants
+
 pipeline_classes = {
     "virsorter": (VirSorterConstants.ADJ_OUT_FILE,),
     "interproscan": (InterproscanConstants.AMENDED_RESULTS_SUFFIX,),
-    "prokka": (),
+    "prokka": (PROKKAConstants.AMENDED_RESULTS_SUFFIX,),
+    "kegg": (BioDataConstants.OUTPUT_SUFFIX,),
+    "peptidase": (CAZYConstants.ASSIGNMENTS_BY_PROTEIN, PeptidaseConstants.EXTRACELLULAR_MATCHES_BYPROT_EXT),
 }
