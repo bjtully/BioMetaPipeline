5c02169bc9caedd94d599e6c45e3e6632484cad6
Christopher Neely <christopher.neely1200@gmail.com>
2019-07-19 20:58:56 -0700

    Add option for passing host directory to pipedm, allowing for correct path reference in inner docker script

--- BioMetaPipeline/Peptidase/psortb.pyx
+++ BioMetaPipeline/Peptidase/psortb.pyx
@@ -39,7 +39,8 @@ class PSORTb(LuigiTaskClass):
         os.makedirs(str(self.output_directory))
         # Version was called from docker installation
         if str(self.docker_path) != "":
-            prot_file = str(self.prot_file).replace("/root/wdir", "/tmp/results")
+            prot_file = str(self.prot_file).replace("/root/wdir", str(self.docker_path))
+            shutil.copy(str(self.prot_file), str(self.output_directory))
             subprocess.run(
                 [
                     "docker",
